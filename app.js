cb7a5445433b854ec845427ca955098225cbfe7e
Chen Ravid <50568384+chendravid@users.noreply.github.com>
2020-05-24 18:03:02 +0300

    Event

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var ua = require('universal-analytics');
 
 //get heroku port or use localhost
 var port = process.env.PORT || 8080;
+
+//init Google analytics
+var visitor = ua('UA-146955087-5');
 
 
 //public stuff
@@ -18,9 +22,11 @@ app.get('/', function(req, res) {
 app.get('/download', function(req, res){
     const file = `${__dirname}/public/xscode-banner.svg`;
     res.download(file); // Set disposition and send it.
+    var origin = req.get('origin');
+    console.log("Origin is:" + origin);
+    visitor.event("Banner", "Download", origin).send()
   });
 
 //start server
-
 app.listen(port);
 console.log ("Running on port 8080");
